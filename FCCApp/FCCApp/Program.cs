2d334489683d1b0f4e367aaeec00a745542e81ab
Thominik <dominikbednarz.ti@gmail.com>
2023-10-17 11:25:54 +0200

    homework day 4

--- FCCApp/FCCApp/Program.cs
+++ FCCApp/FCCApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+bool isMan = false;
+var age = 33;
+
+if (isMan == false && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" &&  age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (isMan == true && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Podaj inne dane");
+}
