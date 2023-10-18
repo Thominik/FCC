7afecfef9838b4f2207b0a8be999fde652584fe1
Thominik <dominikbednarz.ti@gmail.com>
2023-10-18 14:35:28 +0200

    Homework day 5

--- FCCApp/FCCApp/Program.cs
+++ FCCApp/FCCApp/Program.cs
@@ -31,23 +31,79 @@
 
 #region lists
 
-List<string> dayOfWeeks = new List<string>();
-dayOfWeeks.Add("poniedziałek");
-dayOfWeeks.Add("wtorek");
-dayOfWeeks.Add("środa");
-dayOfWeeks.Add("czwartek");
-dayOfWeeks.Add("piątek");
-dayOfWeeks.Add("sobota");
-dayOfWeeks.Add("niedziela");
+//List<string> dayOfWeeks = new List<string>();
+//dayOfWeeks.Add("poniedziałek");
+//dayOfWeeks.Add("wtorek");
+//dayOfWeeks.Add("środa");
+//dayOfWeeks.Add("czwartek");
+//dayOfWeeks.Add("piątek");
+//dayOfWeeks.Add("sobota");
+//dayOfWeeks.Add("niedziela");
 
 //for (var i = 0; i < dayOfWeeks.Count; i++)
 //{
 //    Console.WriteLine(dayOfWeeks[i]);
 //}
 
-foreach (var dayOfWeek in dayOfWeeks)
+//foreach (var dayOfWeek in dayOfWeeks)
+//{
+//    Console.WriteLine(dayOfWeek);
+//}
+
+#endregion
+
+#region homework
+
+int number = 4566;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToArray();
+
+
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+int counter10 = 0;
+
+foreach (char c in letters)
 {
-    Console.WriteLine(dayOfWeek);
+    if (c == '0')
+        counter1++;
+    else if (c == '1')
+        counter2++;
+    else if (c == '2')
+        counter3++; 
+    else if (c == '3')
+        counter4++;
+    else if (c == '4')
+        counter5++;
+    else if (c == '5')
+        counter6++;
+    else if (c == '6')
+        counter7++;
+    else if (c == '7')
+        counter8++;
+    else if (c == '8')
+        counter9++;
+    else if (c == '9')
+        counter10++;
 }
+
+Console.WriteLine($"Wynik dla liczby: {number}");
+Console.WriteLine($"0 => {counter1}");
+Console.WriteLine($"1 => {counter2}");
+Console.WriteLine($"2 => {counter3}");
+Console.WriteLine($"3 => {counter4}");
+Console.WriteLine($"4 => {counter5}");
+Console.WriteLine($"5 => {counter6}");
+Console.WriteLine($"6 => {counter7}");
+Console.WriteLine($"7 => {counter8}");
+Console.WriteLine($"8 => {counter9}");
+Console.WriteLine($"9 => {counter10}");
 
 #endregion
