beabacad330e94d1375c400f346128e7c00157a9
Thominik <dominikbednarz.ti@gmail.com>
2023-10-18 14:19:48 +0200

    Tabs and lists.

--- FCCApp/FCCApp/Program.cs
+++ FCCApp/FCCApp/Program.cs
@@ -1,20 +1,53 @@
-﻿var name = "Ewa";
-bool isMan = false;
-var age = 33;
+﻿#region tabs
+//var grades = new int[365];
 
-if (isMan == false && age < 30)
+//var dayOfWeeks = new string[7];
+
+//dayOfWeeks[0] = "poniedziałek";
+//dayOfWeeks[1] = "wtorek";
+//dayOfWeeks[2] = "środa";
+//dayOfWeeks[3] = "czwartek";
+//dayOfWeeks[4] = "piątek";
+//dayOfWeeks[5] = "sobota";
+//dayOfWeeks[6] = "niedziela";
+
+
+//foreach (var dayOfWeek in dayOfWeeks)
+//{
+//    Console.WriteLine(dayOfWeek);
+//}
+
+//for (var i = 0; i < 7; i++)
+//{
+//    Console.WriteLine(dayOfWeeks[i]);
+//}
+
+//for (var i = 0; i < dayOfWeeks.Length; i++)
+//{
+//    Console.WriteLine(dayOfWeeks[i]);
+//}
+
+#endregion
+
+#region lists
+
+List<string> dayOfWeeks = new List<string>();
+dayOfWeeks.Add("poniedziałek");
+dayOfWeeks.Add("wtorek");
+dayOfWeeks.Add("środa");
+dayOfWeeks.Add("czwartek");
+dayOfWeeks.Add("piątek");
+dayOfWeeks.Add("sobota");
+dayOfWeeks.Add("niedziela");
+
+//for (var i = 0; i < dayOfWeeks.Count; i++)
+//{
+//    Console.WriteLine(dayOfWeeks[i]);
+//}
+
+foreach (var dayOfWeek in dayOfWeeks)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    Console.WriteLine(dayOfWeek);
 }
-else if (name == "Ewa" &&  age == 33)
-{
-    Console.WriteLine("Ewa, lat 33");
-}
-else if (isMan == true && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
-else
-{
-    Console.WriteLine("Podaj inne dane");
-}
+
+#endregion
