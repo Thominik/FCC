572d8fae32b2a2ce3c386e788a87cc55f63dc554
Thominik <dominikbednarz.ti@gmail.com>
2023-10-29 21:30:42 +0100

    Homework day 15

---
+++ FCCApp/FCCApp/IEmployee.cs
@@ -0,0 +1,16 @@
+﻿namespace FCCApp
+{
+    public interface IEmployee
+    {
+        string Name { get; }
+        string Surname { get; }
+        int Age { get; }
+        char Gender { get; }
+        void AddGrade(float grade);
+        void AddGrade(double grade);
+        void AddGrade(int grade);
+        void AddGrade(char grade);
+        void AddGrade(string grade);
+        Statistics GetStatistics();
+    }
+}
--- FCCApp/FCCApp/Supervisor.cs
+++ FCCApp/FCCApp/Supervisor.cs
@@ -1,10 +1,10 @@
 ﻿namespace FCCApp
 {
-    public class Employee : IEmployee
+    public class Supervisor : IEmployee
     {
         private List<float> grades = new List<float>();
 
-        public Employee(string name, string surname, int age, char gender)
+        public Supervisor(string name, string surname, int age, char gender)
         {
             Name = name;
             Surname = surname;
@@ -14,8 +14,8 @@
 
         public string Name { get; private set; }
         public string Surname { get; private set; }
-        public int Age {  get; private set; }
-        public char Gender {  get; private set; }
+        public int Age { get; private set; }
+        public char Gender { get; private set; }
 
         public void AddGrade(float grade)
         {
@@ -27,10 +27,68 @@
 
         public void AddGrade(string grade)
         {
-            if (float.TryParse(grade, out float result))
-                AddGrade(result);
-            else
-                throw new Exception("String is not a float value");
+            switch (grade)
+            {
+                case "6":
+                    AddGrade(100);
+                    break;
+                case "-6":
+                case "6-":
+                    AddGrade(95);
+                    break;
+                case "5+":
+                case "+5":
+                    AddGrade(85);
+                    break;
+                case "5":
+                    AddGrade(80);
+                    break;
+                case "-5":
+                case "5-":
+                    AddGrade(75);
+                    break;
+                case "4+":
+                case "+4":
+                    AddGrade(65);
+                    break;
+                case "4":
+                    AddGrade(60);
+                    break;
+                case "-4":
+                case "4-":
+                    AddGrade(55);
+                    break;
+                case "3+":
+                case "+3":
+                    AddGrade(45);
+                    break;
+                case "3":
+                    AddGrade(40);
+                    break;
+                case "-3":
+                case "3-":
+                    AddGrade(35);
+                    break;
+                case "2+":
+                case "+2":
+                    AddGrade(25);
+                    break;
+                case "2":
+                    AddGrade(20);
+                    break;
+                case "-2":
+                case "2-":
+                    AddGrade(15);
+                    break;
+                case "1+":
+                case "+1":
+                    AddGrade(5);
+                    break;
+                case "1":
+                    AddGrade(0);
+                    break;
+                default: throw new Exception("Invalid value");
+            }
         }
 
         public void AddGrade(char grade)
